1781804c3f91de890b391fdad3763635ac3ecf0d
MuskanJaiswal1 <muskanj9161@gmail.com>
2026-02-06 21:31:53 +0530

    redis connection upstash fixed

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,9 +1,13 @@
 import Redis from "ioredis";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const redis = new Redis(process.env.REDIS_URL!, {
+  tls: {},
   maxRetriesPerRequest: null,
   enableReadyCheck: false,
-  reconnectOnError: () => true
+  reconnectOnError: () => true,
 });
 
 redis.on("connect", () => {
